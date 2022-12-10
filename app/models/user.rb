ac403eb6680c8ea13c0b24ffad1f2436f983ce3d
steponmyfield <claindonstudent@gmail.com>
2022-12-10 19:56:52 +0900

    ゲストユーザーのアカウント情報を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  mount_uploader :profile_image, ProfileimageUploader
+
+  has_many :reviews, dependent: :destroy
+  has_many :records, dependent: :destroy
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.username = 'ゲスト'
+      user.profile_image = 'log.jpeg'
+    end
+  end
 end
