8372dd5f1fe4047680f9b48de584a539e4b6ebf3
steponmyfield <claindonstudent@gmail.com>
2022-12-13 00:16:56 +0900

    スキーマファイル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_115637) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_063153) do
+  create_table "animations", force: :cascade do |t|
+    t.string "animation_name"
+    t.string "animation_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "synopsis"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.string "record_animationname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "animation_id", null: false
+    t.string "content"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["animation_id"], name: "index_reviews_on_animation_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_115637) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "records", "users"
+  add_foreign_key "reviews", "animations"
+  add_foreign_key "reviews", "users"
 end
