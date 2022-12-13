4f9c9d0f991fb631bbb7bc495a495c74a61ec6d1
steponmyfield <claindonstudent@gmail.com>
2022-12-13 23:13:28 +0900

    [update]メニュー開閉のコード修正 閉じるときのボタンを設置したかったのでそれに伴ってコードを変更しました。

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,9 +1,10 @@
-function js_event() {
-  const nav = document.getElementById('click');
-  
-  if (nav.style.display == 'block'){
-    nav.style.display = 'none';
-  }else{
-    nav.style.display = 'block';
-  }
-}
+const modalOpen = document.querySelector(".open")
+const modal = document.querySelector(".header-nav-menu")
+const modalClose = document.querySelector(".close")
+
+modalOpen.addEventListener("click", function () {
+  modal.classList.add("open-modal")
+})
+modalClose.addEventListener("click", function () {
+  modal.classList.remove("open-modal")
+})
