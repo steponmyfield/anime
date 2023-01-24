49f89e0a5f0cb18891aa7d20142960351d1b1bf4
steponmyfield <claindonstudent@gmail.com>
2023-01-24 19:53:12 +0900

    [add]usersコントローラ
    ゲストログインのアクションを追加してます。

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  def account
+    @user = current_user
+  end
+
+  def profile
+    @user = current_user
+  end
+
+  def create
+    @user = User.new(profile_image: 'default_user.jpg')
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params) && @user.valid?(:step1)
+      redirect_to root_path
+    else
+      flash.now[:alert] = "ユーザー名は10文字以内で設定してください"
+      render :profile
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:profile_image, :username)
+  end
+end
