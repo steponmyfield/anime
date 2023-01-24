20bfd70b9d9b134bd4b68a54ca28a1296ba0f400
steponmyfield <claindonstudent@gmail.com>
2023-01-24 20:07:24 +0900

    [update]バリデーション追加
    記録時に作品名を必ず記入するよう設定しました。

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,7 @@
 class Record < ApplicationRecord
   belongs_to :user
 
-  validates :record_animationname, presence: true
+  validates :record_animationname, presence: { message: 'を入力してください。' }
 
   scope :nameorder, -> { order(:record_animationname) }
   scope :lastorder, -> { order(created_at: :DESC) }
